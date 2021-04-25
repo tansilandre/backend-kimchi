c3a41a5b75ce562837d01b59bdce4c225f25ec0a
tansilandre <61400543+tansilandre@users.noreply.github.com>
2021-04-25 10:56:02 +0700

    del

--- routes/kimchi.js
+++ routes/kimchi.js
@@ -1,22 +1,24 @@
 var express = require('express');
 var router = express.Router();
-
 var request = require('request');
 
 /* GET users listing. */
-console.log("mskgan")
-router.get('/', function(req, res, next) {
-  let coin = req.query.coin.toUpperCase()
-  console.log(coin)
+var getGopax = function (req, res, next) {
   request(
     { url: 'https://api.gopax.co.kr/trading-pairs/'+coin+'-KRW/ticker' },
     (err, response, body) => {
       if (err || response.statusCode !== 200) {
         return res.status(500).json({ type: 'error', message: err });
       }
-      res.json(JSON.parse(body));
+      return res.json(JSON.parse(body));
     }
   )
+  next()
+}
+
+router.get('/', function(req, res, next) {
+  let coin = req.query.coin.toUpperCase()
+  getGopax
   // res.send('respond with a resource');
 });
 
