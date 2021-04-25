d44c9ee8cdf4fe7c3663bff4e83c768fdc60b35a
tansilandre <61400543+tansilandre@users.noreply.github.com>
2021-04-25 10:14:14 +0700

    update CORS

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var kimchiRouter = require('./routes/kimchi');
+var cors = require('cors');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/kimchi', kimchiRouter);
+app.use(cors());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
